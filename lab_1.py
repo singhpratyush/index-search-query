52801263caedf666ae2cd9c76f8719666a09f821
Pratyush Singh <singh.pratyush96@gmail.com>
2017-10-07 19:06:19 +0530

    Save the index and reload from disk

--- lab_1.py
+++ lab_1.py
@@ -1,4 +1,5 @@
 import sys
+import pickle
 import magazine_index
 
 
@@ -7,9 +8,13 @@ def main():
         path = sys.argv[1]
         print('Creating index from data at %s' % path)
         index = magazine_index.create_dictionary_index(path)
-        print(index)
-    else:
-        print('Loading existing data from index.bin')
+        print('Saving index to "index.bin"')
+        with open('index.bin', 'wb') as f:
+            pickle.dump(index, f)
+    print('Loading index from "index.bin"')
+    with open('index.bin', 'rb') as f:
+        index = pickle.load(f)
+    print(index)
 
 
 if __name__ == '__main__':
